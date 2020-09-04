60cd6e7d10e7140c9c78ef06daa1e17939498f97
horstslayther <funzi@gmx.de>
2020-09-04 22:24:22 +0200

    richtung

--- main.cpp
+++ main.cpp
@@ -12,8 +12,19 @@ void clear_screen()
   #endif
 }
 void start(){
+  char richtung;
   std::cout << "Du stehst auf einer Strasse und vor dir gabelt sich ein Weg nach links und rechts" << '\n';
   std::cout << "Wohin möchtest du gehen ? (L) oder (R)" << '\n';
+  std::cin >> richtung;
+  switch (richtung) {
+    case 'L':
+      std::cout << "Du siehst einen schoenen Wald\n";
+      break;
+    case 'R':
+        std::cout << "Vor dir breitet sich eine flache Ebene aus, du siehst hohe Berge am Horizont\n";
+        break;
+
+  }
 
 }
 int main() {
